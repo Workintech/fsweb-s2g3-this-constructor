1880171eb526cb17cd981f691bbec0d3c70d1348
Gökhan Özdemir <gokhanozdemir@users.noreply.github.com>
2023-03-01 00:12:07 +0300

    Anlatım ve fonksiyon isimleri güncellendi

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// Aşağıdaki görevleri tamamlayın
+// Aşağıdaki görevleri tamamla
 
 /*
   GÖREV 1
-    - Bir Kisi Constructor'ı yazın 'isim ve 'yas' argümanları alsın.
-    - Tüm Kisi örnekleri boş bir `mide` dizisi ile yüklensin.
-    - Kisi örneklerine `.ye("birYemek")` yeteneği ekleyin:
-        + .ye() parametre olarak bir string almalıdır, bu parametre yenilebilir bir yiyeceği temsil eder
+    - Bir `Kisi` Constructor'ı yaz, 'isim ve 'yas' argümanları alsın.
+    - Tüm `Kisi` örnekleri boş bir `mide` dizisi ile yüklensin.
+    - `Kisi` örneklerine `.ye("birYemek")` yeteneği ekle:
+        + `.ye()` parametre olarak bir string alır, bu parametre bir yiyeceği temsil eder
         + Bir yiyecek yenildiğinde, `mide` dizisine eklenir.
-        + Mide dizisinde 10 eleman varsa `ye` metodu hiçbir etki yapmaz.
-    - Kisi örneklerine `.bosalt()` yeteneği ekleyin:
+        + Eğer, `mide` dizisinde 10 eleman varsa `.ye()` metodu hiçbir etki yapmamalı.
+    - Sonra, `Kisi` örneklerine `.bosalt()` yeteneği de ekleyin:
         + Eğer bosalt tetiklenirse, 'mide' dizisi boşaltılır.
-    - Kisi örneklerine `.toString()` metodu ekleyin:
-        + Bu `isim` ve `yas` i içeren bir string döndürmelidir Örnek: "Mary, 50"
+    - `Kisi` örneklerine `.toString()` metodu ekleyin:
+        + Bu metotla, `isim` ve `yas`i içeren bir string döndürmelisin. (Örn: "Mary, 50")
 */
 
 function Kisi(/* kodlar buraya */) {
@@ -21,18 +21,18 @@ function Kisi(/* kodlar buraya */) {
 
 /*
   GÖREV 2
-    - Bir Araba constructoru yazın, parametre olarak `model` ve `milesPerGallon` alsın.
-    - Tüm Araba örnekleri:
-        + `tank` ı 0 olarak yüklensin
-        + `odometer` ı 0 olarak yüklensin
-    - Arabalara `.fill(gallons)` metoduyla depoyu fulleme kabiliyeti ekleyin
-      + 'gallons', galon sayısını parametre olarak alacak
-      + girilen galonu `tank` a ekleyecek.
-    - ESNEK: Arabalara `.drive(distance)` yeteneği ekleyin. Distance ile ilgili:
-        + `odometer` ın yükselmesine neden olmalı.
-        + `MilesPerGallon` hesaba katılarak `tankın` düşmesine neden olmalıdır.
-    - ESNEK: Bir arabanın benzini biterse drive metodu etkisiz şunu döndürmelidir:
-        +  "x milde benzinim bitti!" x değişkeni `odometer` daki sayı olmalıdır.
+    - Bir `Araba` constructoru yaz ve parametre olarak `model` ve `milesPerGallon` kullan.
+    - Tüm `Araba` örneklerinin:
+        + `benzinDeposu` 0 olarak yüklenir
+        + `kilometreSaati`i de 0 olarak yüklen,r
+    - `Araba`ların `.fill(litre)` metoduyla benzinliğe gidip `benzinDeposu` doldurmasını sağla.
+      + Bu metot, 'litre' parametresiyle yüklenen yakıt miktarını alır.
+      + girilen `litre`yi de `benzinDeposu`na ekler.
+    - BONUS: `Araba`lara `.kullan(mesafe)` özelliği de ekleyin. `mesafe`:
+        + `kilometreSaati`nin artmasına neden olmalı.
+        + `YakitTuketimi` de hesaba katılarak `benzinDeposuın` azalmasını sağlamalıdır.
+    - BONUS: Bir `araba`nın benzini biterse, `kullan` metodu aşağıdaki mesajı paylaşır:
+        +  "x kilometrede benzinim bitti!" x değişkeni `kilometreSaati`ndeki sayı olmalıdır.
 */
 
 function Araba(/* kodlar buraya */) {
@@ -42,7 +42,7 @@ function Araba(/* kodlar buraya */) {
 
 /* 
   GÖREV 3
-  Kendi cümlelerinizle "this" kelimesinin 4 prensibini açıklayın:
+  Kendi cümlelerinle "this" kelimesinin 4 prensibini açıklayın:
   1. 
   2. 
   3. 
@@ -58,7 +58,7 @@ function as(){
 as();
 module.exports = {
   as,
-  Kisi, 
-  Araba,
-  /* Bebek */
+  `Kisi`, 
+  'Araba,
+  
 }
